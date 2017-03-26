697cdaa24febd4bbd9ad126c504ee2e08baa2f9f
zetavg <mail@zeta.vg>
2017-03-26 18:29:34 +0800

    build .books-shelf

--- app/models/book_info/cover_image.rb
+++ app/models/book_info/cover_image.rb
@@ -6,6 +6,13 @@ class BookInfo::CoverImage < ApplicationRecord
 
   validate :immutable_isbn, on: :update
 
+  def dimensions
+    width_scale = 296.0 / width
+    height_scale = 420.0 / height
+    min_scale = [width_scale, height_scale].min
+    [width, height].map { |i| i * min_scale }.map(&:to_i)
+  end
+
   private
 
   def immutable_isbn
