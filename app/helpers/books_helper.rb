6df7775cc248721b774c2e3ce43d5904f49380a8
zetavg <mail@zeta.vg>
2017-03-30 18:19:56 +0800

    style the books#show page

---
+++ app/helpers/books_helper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+module BooksHelper
+  def book_cover(book, responsive: false, style: nil)
+    if book.cover_image.present?
+      content_tag(
+        :div,
+        class: ['book-cover', responsive && 'book-cover-responsive'],
+          style: style,
+        'data-width': book.cover_image.dimensions[0],
+        'data-height': book.cover_image.dimensions[1]
+      ) do
+        image_tag(book.cover_image&.image&.medium&.url)
+      end
+    else
+      content_tag(
+        :div,
+        class: ['book-cover', responsive && 'book-cover-responsive'],
+          style: style,
+        'data-width': 300,
+        'data-height': 420
+      ) do
+        content_tag(:div, class: 'title') do
+          book.name
+        end
+      end
+    end
+  end
+end
